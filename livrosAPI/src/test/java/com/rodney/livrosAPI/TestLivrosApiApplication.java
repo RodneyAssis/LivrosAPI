0e4e5747366edf91af3d240e1d394329b513ccf1
Rodney <rodney.assis@hotmail.com>
2024-03-23 21:24:14 -0300

    git init

---
+++ livrosAPI/src/test/java/com/rodney/livrosAPI/TestLivrosApiApplication.java
@@ -0,0 +1,23 @@
+package com.rodney.livrosAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestLivrosApiApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(LivrosApiApplication::main).with(TestLivrosApiApplication.class).run(args);
+	}
+
+}
